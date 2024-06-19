564f6ee7f133c82ff403cb54c1934da62a9797c9
ruffles182 <l.salvador.arreola@gmail.com>
2024-06-19 02:42:07 -0600

    corrrecion error modelo relaciones properties

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -9,16 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Property extends Model
 {
     use HasFactory;
-
-    public function vendedor()
-    {
-        return $this->belongsTo(Vendedor::class);
-    }
-
-    public function agencia()
-    {
-        return $this->belongsTo(Agencia::class);
-    }
+    
     public function getDaysFromCreatedAttribute()
     {
         return intval((now()->diffInDays($this->date_listed)) * -1);
